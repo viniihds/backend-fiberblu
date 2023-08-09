02a9cf30e6ebea2c45bb577c5a56a2c738d40a58
viniihds <viniciushsilva2210@gmail.com>
2023-08-09 11:27:23 -0300

    Models Produto LinhaProduto GrupoProduto

--- fiberblu/admin.py
+++ fiberblu/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import CategoriaProduto
+from .models import CategoriaProduto, GrupoProduto, LinhaProduto, Produto
 
 admin.site.register(CategoriaProduto)
+admin.site.register(GrupoProduto)
+admin.site.register(LinhaProduto)
+admin.site.register(Produto)
--- fiberblu/models.py
+++ fiberblu/models.py
@@ -11,4 +11,28 @@ class LinhaProduto (models.Model):
 
     def __str__(self):
         return self.descricao
-        
+
+class GrupoProduto (models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+class Produto (models.Model):
+    cor = models.CharField(max_length=100)
+    volume = models.CharField(max_length=100)
+    codigo = models.IntegerField(null = False)
+    preco = models.DecimalField(max_digits=8, null = False, decimal_places = 2)
+    categoria = models.ForeignKey(
+        CategoriaProduto, on_delete=models.PROTECT, related_name="produtos"
+    )
+    linha = models.ForeignKey(
+        LinhaProduto, on_delete=models.PROTECT, related_name="produtos"
+    )
+    grupo = models.ForeignKey(
+        GrupoProduto, on_delete=models.PROTECT, related_name="produtos"
+    )
+
+
+    def __str__(self):
+        return f"{self.categoria} {self.cor} {self.volume}"
