36cd388804050fc90768fc2136ee422463e4444b
viniihds <viniciushsilva2210@gmail.com>
2023-08-09 11:09:57 -0300

    Criação do Backend do Projeto Integrador + models

---
+++ fiberblu/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class CategoriaProduto (models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+class LinhaProduto (models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+        
