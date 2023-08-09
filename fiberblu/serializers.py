e9ee937c0d823c8cf4b5942318b3ca600c4407f1
viniihds <viniciushsilva2210@gmail.com>
2023-08-09 13:24:55 -0300

    Serializer e View LinhaProduto

--- fiberblu/serializers.py
+++ fiberblu/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 
-from fiberblu.models import CategoriaProduto
+from fiberblu.models import CategoriaProduto, LinhaProduto
 
 class CategoriaProdutoSerializer(ModelSerializer):
     class Meta:
         model = CategoriaProduto
+        fields = "__all__"
+
+class LinhaProdutoSerializer(ModelSerializer)
+    class Meta:
+        model = LinhaProduto
         fields = "__all__"
 
--- fiberblu/views.py
+++ fiberblu/views.py
@@ -1,8 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 
-from fiberblu.models import CategoriaProduto
-from fiberblu.serializers import CategoriaProdutoSerializer
+from fiberblu.models import CategoriaProduto, LinhaProduto
+from fiberblu.serializers import CategoriaProdutoSerializer, LinhaProduto
 
 class CategoriaProdutoViewSet(ModelViewSet):
     queryset = CategoriaProduto.objects.all()
     serializer_class = CategoriaProdutoSerializer
+
+class LinhaProdutoViewSet(ModelViewSet):
+    queryset = LinhaProduto.objects.all()
+    serializer_class = LinhaProdutoSerializer
