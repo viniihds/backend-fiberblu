1c7742ac52c44e31983a5f9b842fcd812969ea8d
viniihds <viniciushsilva2210@gmail.com>
2023-08-28 10:41:05 -0300

    Criação ItensCompra

--- fiberblu/admin.py
+++ fiberblu/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CategoriaProduto, GrupoProduto, LinhaProduto, Produto, CategoriaEmpresa, Empresa, Representante, Pedido, Compra
+from .models import CategoriaProduto, GrupoProduto, LinhaProduto, Produto, CategoriaEmpresa, Empresa, Representante, Pedido, Compra, ItensCompra
 
 admin.site.register(CategoriaProduto)
 admin.site.register(GrupoProduto)
@@ -11,3 +11,4 @@ admin.site.register(Empresa)
 admin.site.register(Representante)
 admin.site.register(Pedido)
 admin.site.register(Compra)
+admin.site.register(ItensCompra)
