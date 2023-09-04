f3a6df3129ed532b50ea942b5afd097366b0cf75
viniihds <viniciushsilva2210@gmail.com>
2023-09-04 10:45:23 -0300

    Inclusão do status da compra na listagem da compra

--- fiberblu/serializers/compra.py
+++ fiberblu/serializers/compra.py
@@ -3,6 +3,7 @@ from rest_framework.serializers import ModelSerializer, CharField
 from fiberblu.models import Compra
 
 class CompraSerializer(ModelSerializer):
+    status = CharField(source="get_status_display", read_only=True)
     usuario = CharField(source="usuario.email", read_only=True)
     class Meta:
         model = Compra
