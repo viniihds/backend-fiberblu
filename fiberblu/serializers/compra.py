9d00880f43e589799062c14400390b5bbeb12edf
viniihds <viniciushsilva2210@gmail.com>
2023-09-04 10:47:24 -0300

    Visualização dos itens da compra em compras

--- fiberblu/serializers/compra.py
+++ fiberblu/serializers/compra.py
@@ -1,10 +1,19 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
-from fiberblu.models import Compra
+from fiberblu.models import Compra, ItensCompra
+
+
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = "__all__"
 
 class CompraSerializer(ModelSerializer):
     status = CharField(source="get_status_display", read_only=True)
     usuario = CharField(source="usuario.email", read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
     class Meta:
         model = Compra
         fields = "__all__"
+
