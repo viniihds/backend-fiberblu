a6f8020648d3411888288eaeb23723e7c55603ca
viniihds <viniciushsilva2210@gmail.com>
2023-09-04 10:44:39 -0300

    Inclusão do email do usuário na listagem da compra

--- fiberblu/serializers/compra.py
+++ fiberblu/serializers/compra.py
@@ -1,8 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
 from fiberblu.models import Compra
 
 class CompraSerializer(ModelSerializer):
+    usuario = CharField(source="usuario.email", read_only=True)
     class Meta:
         model = Compra
         fields = "__all__"
