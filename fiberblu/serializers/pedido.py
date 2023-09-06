f92a6daf97fa83bdc5c6d4eaf9b0827df24ed7ca
larissabileski <laricristinabileski@gmail.com>
2023-09-06 12:19:58 -0300

    Part 1 - Finalizada

--- fiberblu/serializers/pedido.py
+++ fiberblu/serializers/pedido.py
@@ -1,9 +1,10 @@
 from rest_framework.serializers import ModelSerializer, CharField
 from fiberblu.models.pedido import Pedido
+from .produto import ProdutoSerializer
 
 class PedidoSerializer(ModelSerializer):
-    # produtos = CharField(source = 'produto.categoria produto.cor produto.volume')
-    # representante = CharField(source='representante.nome')
+    produtos = ProdutoSerializer(many=True)
+    representante = CharField(source='representante.nome')
     empresa = CharField(source='empresa.nome')
     class Meta:
         model = Pedido
