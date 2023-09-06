4b17b5e4791094bd3500fe2eb046dce968972e39
larissabileski <laricristinabileski@gmail.com>
2023-09-06 12:11:50 -0300

    Erro corrigido

--- fiberblu/serializers/pedido.py
+++ fiberblu/serializers/pedido.py
@@ -2,8 +2,8 @@ from rest_framework.serializers import ModelSerializer, CharField
 from fiberblu.models.pedido import Pedido
 
 class PedidoSerializer(ModelSerializer):
-    produtos = CharField(source = 'produto.categoria produto.cor produto.volume')
-    representante = CharField(source='representante.nome')
+    # produtos = CharField(source = 'produto.categoria produto.cor produto.volume')
+    # representante = CharField(source='representante.nome')
     empresa = CharField(source='empresa.nome')
     class Meta:
         model = Pedido
--- fiberblu/serializers/produto.py
+++ fiberblu/serializers/produto.py
@@ -1,6 +1,10 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 from fiberblu.models.produto import Produto
+
 class ProdutoSerializer(ModelSerializer):
+    linha = CharField(source='linha.descricao')
+    grupo = CharField(source='grupo.descricao')
+    categoria = CharField(source='categoria.descricao')
     class Meta:
         model = Produto
         fields = "__all__"
