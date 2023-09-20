1cb97a90863d50a3a502cb97a97c1f949618c51b
larissabileski <laricristinabileski@gmail.com>
2023-09-20 11:29:04 -0300

    Alteração na Model Produto

--- fiberblu/models/produto.py
+++ fiberblu/models/produto.py
@@ -8,7 +8,6 @@ from .linhaProduto import LinhaProduto
 class Produto(models.Model):
     cor = models.CharField(max_length=100)
     volume = models.IntegerField(null=False)
-    codigo = models.IntegerField(null=False)
     preco = models.DecimalField(max_digits=8, null=False, decimal_places=2)
     categoria = models.ForeignKey(CategoriaProduto, on_delete=models.PROTECT, related_name="produtos")
     linha = models.ForeignKey(LinhaProduto, on_delete=models.PROTECT, related_name="produtos")
