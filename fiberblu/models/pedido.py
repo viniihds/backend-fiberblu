d954510fab20191abb79c05a9e10f432cdacd2c1
larissabileski <laricristinabileski@gmail.com>
2023-09-25 10:51:58 -0300

    Alteração na Model Pedido, data em DateField

--- fiberblu/models/pedido.py
+++ fiberblu/models/pedido.py
@@ -9,7 +9,7 @@ class Pedido(models.Model):
     produtos = models.ManyToManyField(Produto, related_name="pedidos")
     representante = models.ForeignKey(Representante, on_delete=models.PROTECT, related_name="pedidos")
     valor = models.DecimalField(max_digits=10, null=False, decimal_places=2)
-    data = models.CharField(max_length=10)
+    data = models.DateField(max_length=10)
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, related_name="pedidos")
 
     def __str__(self):
