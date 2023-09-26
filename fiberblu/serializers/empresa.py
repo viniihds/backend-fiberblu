c96ae039704916a5782da0aa2af41d2dae861884
larissabileski <laricristinabileski@gmail.com>
2023-09-26 17:02:03 -0300

    Categoria Empresa atualizada

--- fiberblu/serializers/empresa.py
+++ fiberblu/serializers/empresa.py
@@ -1,9 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
 from fiberblu.models.empresa import Empresa
 
-
 class EmpresaSerializer(ModelSerializer):
+    categoria = CharField(source="categoria.descricao")
     class Meta:
         model = Empresa
         fields = "__all__"
