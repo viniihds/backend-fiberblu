ca5a19517a76981138e39ae9d7cfb4f22173302e
larissabileski <laricristinabileski@gmail.com>
2023-09-27 11:25:17 -0300

    campo Representante removido da Model Pedidos

--- fiberblu/serializers/pedido.py
+++ fiberblu/serializers/pedido.py
@@ -7,7 +7,6 @@ from .produto import ProdutoSerializer
 
 class PedidoSerializer(ModelSerializer):
     produtos = ProdutoSerializer(many=True)
-    representante = CharField(source="representante.nome")
     usuario = CharField(source="usuario.email")
     empresa = CharField(source="empresa.nome")
     status = CharField(source="get_status_display")
