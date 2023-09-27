19844d8be6d04cd340a15ab2d9505392a8dca1e2
larissabileski <laricristinabileski@gmail.com>
2023-09-27 11:23:47 -0300

    Status do pedido e usuario adicionado a Model Pedido

--- fiberblu/models/pedido.py
+++ fiberblu/models/pedido.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from usuario.models import Usuario
+
 from .empresa import Empresa
 from .produto import Produto
 from .representante import Representante
@@ -7,6 +9,14 @@ from .pagamento import Pagamento
 
 
 class Pedido(models.Model):
+    class StatusPedido(models.IntegerChoices):
+        CARRINHO = (1,"Carrinho",)
+        REALIZADO = (2,"Realizado",)
+        PAGO = (3,"Pago",)
+        ENTREGUE = (4,"Entregue",)
+
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name="compras")
+    status = models.IntegerField(choices=StatusPedido.choices,  default=StatusPedido.CARRINHO)
     produtos = models.ManyToManyField(Produto, related_name="pedidos")
     representante = models.ForeignKey(Representante, on_delete=models.PROTECT, related_name="pedidos")
     valor = models.DecimalField(max_digits=10, null=False, decimal_places=2)
--- fiberblu/serializers/pedido.py
+++ fiberblu/serializers/pedido.py
@@ -8,7 +8,9 @@ from .produto import ProdutoSerializer
 class PedidoSerializer(ModelSerializer):
     produtos = ProdutoSerializer(many=True)
     representante = CharField(source="representante.nome")
+    usuario = CharField(source="usuario.email")
     empresa = CharField(source="empresa.nome")
+    status = CharField(source="get_status_display")
     pagamento = CharField(source="pagamento.descricao")
 
     class Meta:
