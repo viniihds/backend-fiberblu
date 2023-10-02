fc6c2a03112be3916592a8b6325a7a262331543c
larissabileski <laricristinabileski@gmail.com>
2023-10-02 10:08:29 -0300

    Remoção da View Compra

--- fiberblu/views/compra.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-
-from fiberblu.models import Compra
-from fiberblu.serializers import CompraSerializer
-
-
-class CompraViewSet(ModelViewSet):
-    queryset = Compra.objects.all()
-    serializer_class = CompraSerializer
