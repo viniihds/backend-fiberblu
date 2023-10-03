93816334f782fb21ff15a46f2f310aa72f06b203
larissabileski <laricristinabileski@gmail.com>
2023-10-03 15:07:15 -0300

    Inicio da Implantação no Fl0

--- fiberblu/admin.py
+++ fiberblu/admin.py
@@ -18,3 +18,32 @@ class ItensPedidoInline(admin.TabularInline):
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
     inlines = [ItensPedidoInline]
+
+# @admin.register(Autor)
+# class AutorAdmin(admin.ModelAdmin):
+#     list_display = ('nome', 'email')
+#     search_fields = ('nome', 'email')
+#     list_filter = ('nome',)
+#     ordering = ('nome', 'email')
+
+# @admin.register(Categoria)
+# class CategoriaAdmin(admin.ModelAdmin):
+#     list_display = ('descricao',)
+#     search_fields = ('descricao',)
+#     list_filter = ('descricao',)
+#     ordering = ('descricao',)
+
+# @admin.register(Editora)
+# class EditoraAdmin(admin.ModelAdmin):
+#     list_display = ('nome',)
+#     search_fields = ('nome',)
+#     list_filter = ('nome',)
+#     ordering = ('nome',)
+
+# @admin.register(Livro)
+# class LivroAdmin(admin.ModelAdmin):
+#     list_display = ('titulo', 'editora', 'categoria')
+#     search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
+#     list_filter = ('editora', 'categoria')
+#     ordering = ('titulo', 'editora', 'categoria')
+#     list_per_page = 25
