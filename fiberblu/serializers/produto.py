8988092ce5333b33ed834e029521d8b08554ff35
viniihds <viniciushsilva2210@gmail.com>
2023-12-11 21:52:32 -0300

    Teste

--- fiberblu/serializers/produto.py
+++ fiberblu/serializers/produto.py
@@ -1,10 +1,33 @@
 from rest_framework.serializers import CharField, ModelSerializer
-
+from django.db import models
 from fiberblu.models.produto import Produto
+from fiberblu.models.produto import CategoriaProduto
+from fiberblu.models.produto import LinhaProduto
+from fiberblu.models.produto import GrupoProduto
+
 
 
 class ProdutoSerializer(ModelSerializer):
 
+
+    class Meta:
+        model = Produto
+        fields = "__all__"
+
+class ProdutooDetailSerializer(ModelSerializer):
+
+    class Meta:
+        model = Produto
+        fields = "__all__"
+        depth = 1
+
+class ProdutoListSerializer(ModelSerializer):
+    categoria = models.ForeignKey(CategoriaProduto, on_delete=models.PROTECT, related_name="produtos")
+    volume = models.IntegerField(null=False)
+    preco = models.DecimalField(max_digits=8, null=False, decimal_places=2)
+    categoria = models.ForeignKey(CategoriaProduto, on_delete=models.PROTECT, related_name="produtos")
+    linha = models.ForeignKey(LinhaProduto, on_delete=models.PROTECT, related_name="produtos")
+    grupo = models.ForeignKey(GrupoProduto, on_delete=models.PROTECT, related_name="produtos")
 
     class Meta:
         model = Produto
